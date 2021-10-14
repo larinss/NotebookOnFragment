5c4a84df2d7450da9efec56261895742ab3a1be8
larinss <larinss.new@gmail.com>
2021-10-14 18:17:39 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/course1/notebookonfragment/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.gb.course1.notebookonfragment.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.gb.course1.notebookonfragment.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
